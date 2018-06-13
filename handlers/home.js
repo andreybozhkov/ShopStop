0324b1bfcd5f1e23213c7da895126403c9937721
andreybozhkov <bozhkov.andrey@gmail.com>
2018-06-13 20:10:28 +0300

    ADDED 03.06. Category creation

--- handlers/home.js
+++ handlers/home.js
@@ -1,7 +1,6 @@
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-//const database = require('../config/database');
 const qs = require('querystring');
 
 const Product = require('../models/Product');
