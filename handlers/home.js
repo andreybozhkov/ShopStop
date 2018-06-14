629e82341629557eb7a0046addf752ec0eb26740
andreybozhkov <bozhkov.andrey@gmail.com>
2018-06-14 19:27:07 +0300

    UPDATE Converted to Express initial set-up 04.01.

--- handlers/home.js
+++ handlers/home.js
@@ -1,60 +1,49 @@
-const url = require('url');
 const fs = require('fs');
 const path = require('path');
-const qs = require('querystring');
-
 const Product = require('../models/Product');
 
-module.exports = (req, res) => {
-    req.pathname = req.pathname || url.parse(req.url).pathname;
+module.exports.index = (req, res) => {
+    let filePath = path.normalize(
+        path.join(__dirname, '../views/home/index.html'));
+    
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
 
-    if (req.pathname === '/' && req.method === 'GET'){
-        let filePath = path.normalize(
-            path.join(__dirname, '../views/index.html'));
-            
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.log(err);
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
+            res.write('404 not found!');
+            res.end();
+            return;
+        }
 
-                res.write('404 not found!');
-                res.end();
-                return;
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+
+        let queryData = req.query;
+
+        Product.find().then((products) => {
+            if (queryData.query) {
+                products = products.filter(
+                    p => p.name.toLowerCase()
+                                .includes(queryData.query));
             }
 
-            let queryData = qs.parse(url.parse(req.url).query);
+            let content = '';
+            for (let product of products) {
+                content +=
+                `<div class="product-card">
+                    <img class="product-img" src="${product.image}">
+                    <h2>${product.name}</h2>
+                    <p>${product.description}</p>
+                </div>`;
+            }
 
-            Product.find().then((products) => {
-                if (queryData.query) {
-                    products = products.filter(
-                        p => p.name.toLowerCase()
-                                    .includes(queryData.query));
-                }
-
-                let content = '';
-                for (let product of products) {
-                    content +=
-                    `<div class="product-card">
-                        <img class="product-img" src="${product.image}">
-                        <h2>${product.name}</h2>
-                        <p>${product.description}</p>
-                        <p>${product.price}</p>
-                    </div>`;
-                }
-    
-                let html = data.toString().replace('{content}', content);
-    
-                res.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                res.write(html);
-                res.end();
-            });
+            let html = data.toString().replace('{content}', content);
+            res.write(html);
+            res.end();
         });
-    }
-    else {
-        return true;
-    }
+    });
 };
