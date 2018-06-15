3e909ef969977081d51a3ea303581607229f4391
andreybozhkov <bozhkov.andrey@gmail.com>
2018-06-15 22:19:14 +0300

    ADDED simple user error/success message system 04.05.

--- handlers/home.js
+++ handlers/home.js
@@ -10,6 +10,13 @@ module.exports.index = (req, res) => {
                             .includes(queryData.query));
         }
 
-        res.render('home/index', {products: products});
+        let data = {products: products};
+        if (req.query.error) {
+            data.error = req.query.error;
+        } else if (req.query.success) {
+            data.success = req.query.success;
+        }
+
+        res.render('home/index', data);
     });
 };
