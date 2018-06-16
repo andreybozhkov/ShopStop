4708eb51d66802cf6c74bdd66ea5d5e00daa8416
andreybozhkov <bozhkov.andrey@gmail.com>
2018-06-16 17:48:07 +0300

    ADDED edit product functionality 04.06.

--- handlers/product.js
+++ handlers/product.js
@@ -19,3 +19,68 @@ module.exports.addPost = (req, res) => {
         res.redirect('/');
     });
 }
+
+module.exports.editGet = (req, res) => {
+    let id = req.params.id;
+    Product.findById(id).then(product => {
+        if (!product) {
+            res.sendStatus(404);
+            return;
+        }
+        
+        Category.find().then((categories) => {
+            res.render('product/edit', {
+                product: product,
+                categories: categories
+            });
+        });
+    });
+};
+
+module.exports.editPost = (req, res) => {
+    let id = req.params.id;
+    let editedProduct = req.body;
+
+    Product.findById(id).then((product) => {
+        if (!product) {
+            res.redirect(
+                `/?error=${encodeURIComponent('Product was not found!')}`);
+            return;
+        }
+
+        product.name = editedProduct.name;
+        product.description = editedProduct.description;
+        product.price = editedProduct.price;
+
+        if (req.file) {
+            product.image = '\\' + req.file.path;
+        }
+
+        if (product.category.toString() !== editedProduct.category) {
+            Category.findById(product.category).then((currentCategory) => {
+                Category.findById(editedProduct.category).then((nextCategory) => {
+                    let index = currentCategory.products.indexOf(product._id);
+                    if (index >= 0) {
+                        currentCategory.products.splice(index, 1);
+                    }
+                    currentCategory.save();
+
+                    nextCategory.products.push(product._id);
+                    nextCategory.save();
+
+                    product.category = editedProduct.category;
+
+                    product.save().then(() => {
+                        res.redirect(
+                            `/?success=${encodeURIComponent('Product was edited sucessfully!')}`);
+                    });
+                });
+            });
+        } else {
+            product.save().then(() => {
+                res.redirect(
+                    `/?success=${encodeURIComponent('Product was edited sucessfully!')}`);
+            });
+        }
+    });
+};
