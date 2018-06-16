8258b3035edb4ff4ce3bfe5e0d70ba76750fe3dd
andreybozhkov <bozhkov.andrey@gmail.com>
2018-06-16 22:52:58 +0300

    ADDED user logout functionality

--- controllers/user.js
+++ controllers/user.js
@@ -40,3 +40,32 @@ module.exports.registerPost = (req, res) => {
             res.render('user/register', user);
         });
 }
+
+module.exports.loginGet = (req, res) => {
+    res.render('user/login');
+}
+
+module.exports.loginPost = (req, res) => {
+    let userToLogin = req.body;
+
+    User.findOne({ username: userToLogin.username }).then(user => {
+        if (!user || !user.authenticate(userToLogin.password)) {
+            res.render('user/login', { error: 'Invalid credentials!' });
+        } else {
+            req.logIn(user, (error, user) => {
+                if (error) {
+                    res.render('user/login',
+                    { error: 'Authentication not working!' });
+                    return;
+                }
+
+                res.redirect('/');
+            });
+        }
+    });
+}
+
+module.exports.logout = (req, res) => {
+    req.logout();
+    res.redirect('/');
+}
