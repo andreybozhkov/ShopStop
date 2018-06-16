b6c5bb463bc8f2f147af3a8de79e4c4bb4490967
andreybozhkov <bozhkov.andrey@gmail.com>
2018-06-16 20:01:49 +0300

    ADDED user authentication modules and set-up

--- config/express.js
+++ config/express.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 
 module.exports = (app, config) => {
     app.set('view engine', 'pug');
@@ -8,6 +11,15 @@ module.exports = (app, config) => {
     
     // Configure middleware for parsing form data.
     app.use(bodyParser.urlencoded({extended: true}));
+    
+    app.use(cookieParser());
+    app.use(session({
+        secret: 'S3cr3t',
+        saveUninitialized: false,
+        resave: false
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     // Configure "public" folder
     app.use((req, res, next) => {
